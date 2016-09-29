e53b7d160ceb7b7c4c52837464f4097b83619962
Altherlex Alves <para.alves@gmail.com>
2016-09-29 17:52:45 -0300

    deployng code

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,10 @@
 Chef::Log.debug('*'*80)
 Chef::Log.debug(node.inspect)
 Chef::Log.debug('*'*80)
+
+
+opsworks_deploy_dir do
+  user config[:user]
+  group config[:group]
+  path config[:deploy_to]
+end
